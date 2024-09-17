5c153c08d7d016d3d52f8dcd2966ac5b0ef8ae53
PetricaH <petricahphp@gmail.com>
2024-09-17 14:51:37 +0300

    update

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="static/css/public_styling.css">
-   
-    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-    
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <title>Wartung</title>
+<?php require_once('config.php') ?>
+<?php require_once( ROOT_PATH . '/includes/head_section.php') ?>
+<?php require_once( ROOT_PATH . '/includes/public_functions.php') ?>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
+     <title>Wartung</title>
 </head>
 <body>
     <section id="hero-section">
